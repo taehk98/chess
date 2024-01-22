5f6180481ca2d9b6069c96972e59743394bedb7e
Taehoon <taehoon7@byu.edu>
2024-01-21 17:08:00 -0700

    Add bishop move calcultor

--- 0-chess-moves/starter-code/chess/shared/src/main/java/chess/ChessBoard.java
+++ 0-chess-moves/starter-code/chess/shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Arrays;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,9 +9,9 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
-
+    private ChessPiece[][] squares = new ChessPiece[9][9];
     public ChessBoard() {
-        
+
     }
 
     /**
@@ -19,7 +21,20 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        squares[position.getRow()][position.getColumn()] = piece;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessBoard that = (ChessBoard) o;
+        return Arrays.equals(squares, that.squares);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(squares);
     }
 
     /**
@@ -30,7 +45,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return squares[position.getRow()][position.getColumn()];
     }
 
     /**
@@ -38,6 +53,6 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        squares = new ChessPiece[8][8];
     }
 }
---
+++ 0-chess-moves/starter-code/chess/shared/src/main/java/chess/PieceMovesCalculators/BishopMovesCalculator.java
@@ -0,0 +1,117 @@
+package chess.PieceMovesCalculators;
+
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPosition;
+import chess.PieceMovesCalculator;
+
+import java.util.Collection;
+import java.util.ArrayList;
+
+public class BishopMovesCalculator implements PieceMovesCalculator {
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
+        Collection<ChessMove> moves = new ArrayList<>();
+
+        int temptRow;
+
+        temptRow = position.getRow();
+        for (int i = position.getColumn()+1; i <= 8; i++) {
+            temptRow--;
+            if (temptRow < 1) {
+                break;
+            }
+            ChessPosition pos = new ChessPosition(temptRow, i);
+            if (pos.getRow() <= 8 && pos.getRow() >= 1 && board.getPiece(pos) == null) {
+                ChessMove move = new ChessMove(position, pos, null);
+                moves.add(move);
+            } // When there is no piece, add the move
+            else if (board.getPiece(pos) != null && (
+                    board.getPiece(pos).getTeamColor() == board.getPiece(position).getTeamColor())) {
+                break;
+            } // When there is one of my pieces
+            else if (board.getPiece(pos) != null && (board.getPiece(pos).getTeamColor()
+                    != board.getPiece(position).getTeamColor())) {
+                ChessMove move = new ChessMove(position, pos, null);
+                moves.add(move);
+                break;
+            } // When there is an enemy's piece, add the move and end the loop;
+        } // bottom right diagonal
+
+
+        temptRow = position.getRow();
+        for (int i = position.getColumn()+1; i <= 8; i++) {
+            temptRow++;
+            if (temptRow > 8) {
+                break;
+            }
+            ChessPosition pos2 = new ChessPosition(temptRow, i);
+            if (pos2.getRow() <= 8 && pos2.getRow() >= 1
+                    && board.getPiece(pos2) == null) {
+                ChessMove move = new ChessMove(position, pos2, null);
+                moves.add(move);
+            } // When there is no piece, add the move
+            else if (board.getPiece(pos2) != null && (
+                    board.getPiece(pos2).getTeamColor() ==
+                            board.getPiece(position).getTeamColor())) {
+
+                break;
+            } // When there is one of my pieces
+            else if (board.getPiece(pos2) != null &&
+                    (board.getPiece(pos2).getTeamColor()
+                            != board.getPiece(position).getTeamColor())) {
+                ChessMove move = new ChessMove(position, pos2, null);
+                moves.add(move);
+                break;
+//            } // When there is an enemy's piece, add the move and end the loop;
+            } // top right diagonal
+        }
+
+        temptRow = position.getRow();
+
+        for (int i = position.getColumn()-1; i >=1; i--) {
+            temptRow--;
+            if (temptRow < 1) {
+                break;
+            }
+            ChessPosition pos3 = new ChessPosition(temptRow, i);
+            if (pos3.getRow() >= 1 && pos3.getRow() <= 8 && board.getPiece(pos3) == null) {
+                ChessMove move = new ChessMove(position, pos3, null);
+                moves.add(move);
+            } // When there is no piece, add the move
+            else if (board.getPiece(pos3) != null && (
+                    board.getPiece(pos3).getTeamColor() == board.getPiece(position).getTeamColor())) {
+                break;
+            } // When there is one of my pieces
+            else if (board.getPiece(pos3) != null && (board.getPiece(pos3).getTeamColor() != board.getPiece(position).getTeamColor())) {
+                ChessMove move = new ChessMove(position, pos3, null);
+                moves.add(move);
+                break;
+            } // When there is an enemy's piece, add the move and end the loop;
+        } // bottom left diagonal
+
+        temptRow = position.getRow();
+        for (int i = position.getColumn()-1; i >= 1; i--) {
+            temptRow++;
+            if (temptRow > 8) {
+                break;
+            }
+            ChessPosition pos4 = new ChessPosition(temptRow, i);
+            if (pos4.getRow() <= 8 && pos4.getRow() >= 1 && board.getPiece(pos4) == null) {
+                ChessMove move = new ChessMove(position, pos4, null);
+                moves.add(move);
+            } // When there is no piece, add the move
+            else if (board.getPiece(pos4) != null && (
+                    board.getPiece(pos4).getTeamColor() == board.getPiece(position).getTeamColor())) {
+                break;
+            } // When there is one of my pieces
+            else if (board.getPiece(pos4) != null && (board.getPiece(pos4).getTeamColor() != board.getPiece(position).getTeamColor())) {
+                ChessMove move = new ChessMove(position, pos4, null);
+                moves.add(move);
+                break;
+                // When there is an enemy's piece, add the move and end the loop;
+            } // top left diagonal
+        }
+        return moves;
+    }
+}
