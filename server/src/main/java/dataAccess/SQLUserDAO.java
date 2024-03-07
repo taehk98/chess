1a429e51be1b0d6a57870302974accd8612cb526
Taehoon <taehoon7@byu.edu>
2024-03-07 10:38:37 -0700

    fixes bugs

--- server/src/main/java/dataAccess/SQLUserDAO.java
+++ server/src/main/java/dataAccess/SQLUserDAO.java
@@ -83,22 +83,19 @@ public class SQLUserDAO implements UserDAO{
         }
     }
 
-    private UserData readUser(ResultSet rs) throws SQLException {
-        var json = rs.getString("json");
-        return new Gson().fromJson(json, UserData.class);
-    }
-
-
-
-
-    private void configureDatabase() throws DataAccessException {
-        var createStatements =
-                """
-                      CREATE TABLE IF NOT EXISTS user(
-                      username VARCHAR(255) NOT NULL,
-                      json TEXT DEFAULT NULL,
-                      PRIMARY KEY (username)
-                      )""";
-        SQLAuthDAO.configureDatabase(createStatements);
-    }
+//    private UserData readUser(ResultSet rs) throws SQLException {
+//        var json = rs.getString("json");
+//        return new Gson().fromJson(json, UserData.class);
+//    }
+//
+//    private void configureDatabase() throws DataAccessException {
+//        var createStatements =
+//                """
+//                      CREATE TABLE IF NOT EXISTS user(
+//                      username VARCHAR(255) NOT NULL,
+//                      json TEXT DEFAULT NULL,
+//                      PRIMARY KEY (username)
+//                      )""";
+//        SQLAuthDAO.configureDatabase(createStatements);
+//    }
 }
