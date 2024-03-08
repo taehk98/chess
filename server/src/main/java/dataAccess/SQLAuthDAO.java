34c2918cc869c6de58b87259da80fc4eeea06b52
Taehoon <taehoon7@byu.edu>
2024-03-07 17:19:16 -0700

    adds authDAO Test

--- server/src/main/java/dataAccess/SQLAuthDAO.java
+++ server/src/main/java/dataAccess/SQLAuthDAO.java
@@ -1,32 +1,27 @@
 package dataAccess;
 
-import com.google.gson.Gson;
 import model.AuthData;
-import model.UserData;
 
-import javax.xml.crypto.Data;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.UUID;
 
-import static java.sql.Statement.RETURN_GENERATED_KEYS;
+public class SQLAuthDAO implements AuthDAO {
 
-public class SQLAuthDAO implements AuthDAO{
-
-    public SQLAuthDAO()  {
+    public SQLAuthDAO() {
         try {
-//            configureDatabase();
             createTable();
-        } catch (DataAccessException e) {
+        } catch (DataAccessException ignored) {
         }
     }
-    public void createTable()throws DataAccessException{
+
+    public void createTable() throws DataAccessException {
         DatabaseManager.createDatabase();
-        try(var conn = DatabaseManager.getConnection()){
+        try (var conn = DatabaseManager.getConnection()) {
 
 
-            var createAuthTokenTable ="""
-              CREATE TABLE IF NOT EXISTS auth(
+            var createAuthTokenTable = """
+              
+                    CREATE TABLE IF NOT EXISTS auth(
                   authToken VARCHAR(255) NOT NULL,
                   username VARCHAR(255) NOT NULL,
                   PRIMARY KEY (authToken)
@@ -35,14 +30,14 @@ public class SQLAuthDAO implements AuthDAO{
             try(var createTableStatement = conn.prepareStatement(createAuthTokenTable)){
                 createTableStatement.executeUpdate();
             }
-        }catch(SQLException e){
+        }catch( SQLException e){
             throw new DataAccessException("Data Access Error");
         }
     }
     @Override
     public AuthData createAuth(String username) throws DataAccessException {
         String token = UUID.randomUUID().toString();
-        var conn = DatabaseManager.getConnection();
+        var conn = DatabaseManager.getConnection ();
         try(var preparedStatement = conn.prepareStatement("INSERT INTO auth (authToken, username) VALUES(?, ?)")){
             preparedStatement.setString(1, token);
             preparedStatement.setString(2, username);
@@ -90,33 +85,4 @@ public class SQLAuthDAO implements AuthDAO{
         }
         return null;
     }
-
-    private AuthData readAuth(ResultSet rs) throws SQLException {
-        var json = rs.getString("json");
-        return new Gson().fromJson(json, AuthData.class);
-    }
-
-
-//    private void configureDatabase() throws DataAccessException {
-//        var createStatements ="""
-//                   CREATE TABLE IF NOT EXISTS auth(
-//                  authToken VARCHAR(255) NOT NULL,
-//                  json TEXT DEFAULT NULL,
-//                  PRIMARY KEY (authToken)
-//                  )""";
-//        configureDatabase(createStatements);
-//    }
-//
-//    static void configureDatabase(String createStatements) throws DataAccessException {
-//
-//        try (var conn = DatabaseManager.getConnection()) {
-//            DatabaseManager.createDatabase();
-//            conn.setCatalog("chess");
-//                try (var preparedStatement = conn.prepareStatement(createStatements)) {
-//                    preparedStatement.executeUpdate();
-//                }
-//        } catch (SQLException ex) {
-//            throw new DataAccessException("Unable to configure database");
-//        }
-//    }
 }
--- server/src/test/java/dataAccessTests/AuthDAOTest.java
+++ server/src/test/java/dataAccessTests/AuthDAOTest.java
@@ -1,4 +1,113 @@
 package dataAccessTests;
 
+import chess.ChessGame;
+import dataAccess.*;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import org.junit.jupiter.api.*;
+
+import java.sql.SQLException;
+
 public class AuthDAOTest {
+    private AuthDAO authDAO;
+
+    @BeforeEach
+    void setUp() {
+        try(var conn = DatabaseManager.getConnection()) {
+            authDAO = new SQLAuthDAO();
+        } catch (SQLException | DataAccessException e) {
+            authDAO = new MemoryAuthDAO();
+        }
+        // UserService 객체 초기화
+    }
+
+    @AfterEach
+    void tearDown() throws DataAccessException {
+        authDAO.clear();
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Positively tests CreateAuth if it creates correctly")
+    public void PosCreateTest() throws DataAccessException {
+        authDAO.clear();
+        AuthData newAuth = authDAO.createAuth("taehoon");
+        Assertions.assertEquals(newAuth.username(), "taehoon");
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Negatively tests CreateAuth if it creates correctly")
+    public void NegCreateTest() throws DataAccessException {
+        AuthData newAuth = authDAO.createAuth("taehoon");
+        Assertions.assertNotNull(newAuth.authToken());
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Positively tests getAuth if it creates correctly")
+    public void PosGetAuthTest() throws DataAccessException {
+        AuthData newAuth = authDAO.createAuth("taehoon");
+        AuthData returnedAuth = authDAO.getAuth(newAuth);
+        Assertions.assertEquals(newAuth.authToken(), returnedAuth.authToken());
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Negatively tests getAuth if it creates correctly")
+    public void NegGetAuthTest() throws DataAccessException {
+        AuthData newAuth = authDAO.createAuth("taehoon");
+        AuthData returnedAuth = authDAO.getAuth(newAuth);
+        Assertions.assertNotNull(returnedAuth.username());
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Tests clear() to see if it clears appropriately")
+    public void AuthClearTest() throws DataAccessException {
+        AuthData newAuth = authDAO.createAuth("taehoon");
+        AuthData anotherAuth = authDAO.createAuth("saehee");
+        AuthData returnedAuth = authDAO.getAuth(newAuth);
+        AuthData anotherReturnedAuth = authDAO.getAuth(anotherAuth);
+        Assertions.assertNotNull(returnedAuth);
+        Assertions.assertNotNull(anotherReturnedAuth);
+
+        authDAO.clear();
+
+        returnedAuth = authDAO.getAuth(newAuth);
+        Assertions.assertNull(returnedAuth);
+        anotherReturnedAuth = authDAO.getAuth(anotherReturnedAuth);
+        Assertions.assertNull(anotherReturnedAuth);
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Positively Tests AuthDelete() to see if it clears appropriately")
+    public void PosAuthDeleteTest() throws DataAccessException {
+        AuthData newAuth = authDAO.createAuth("taehoon");
+        authDAO.createAuth("saehee");
+        AuthData returnedAuth = authDAO.getAuth(newAuth);
+        Assertions.assertNotNull(returnedAuth);
+
+        authDAO.delete(newAuth);
+
+        returnedAuth = authDAO.getAuth(newAuth);
+        Assertions.assertNull(returnedAuth);
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("Negatively Tests AuthDelete() to see if it clears appropriately")
+    public void NegAuthDeleteTest() throws DataAccessException {
+        AuthData newAuth = authDAO.createAuth("taehoon");
+        AuthData anotherAuth = authDAO.createAuth("saehee");
+        AuthData returnedAuth = authDAO.getAuth(newAuth);
+        Assertions.assertNotNull(returnedAuth);
+
+        authDAO.delete(newAuth);
+
+        AuthData anotherReturnedAuth = authDAO.getAuth(anotherAuth);
+        Assertions.assertNotNull(anotherReturnedAuth);
+    }
 }
