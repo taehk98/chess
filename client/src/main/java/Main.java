74d1df5bba78daffa504422bd53dac299f8760ba
Taehoon <taehoon7@byu.edu>
2024-04-07 14:32:51 -0600

    adds changes

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,6 @@
 import Client.Repl;
 import Client.ServerFacade;
 import chess.*;
-import dataAccess.DataAccessException;
-import server.Server;
 import ui.ChessBoardPrinter;
 
 import javax.websocket.DeploymentException;
@@ -10,17 +8,17 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class Main {
-    private static Server server;
+//    private static Server server;
     static ServerFacade facade;
-    public static void main(String[] args) throws DataAccessException, DeploymentException, IOException, URISyntaxException {
-        server = new Server();
-        var port = server.run(0);
-        System.out.println("Started test HTTP server on " + port);
-        String serverUrl = "http://localhost:" + port;
-        facade = new ServerFacade(serverUrl);
-        facade.clear();
-
-        new Repl(serverUrl).run();
+    public static void main(String[] args) throws DeploymentException, IOException, URISyntaxException {
+//        server = new Server();
+//        var port = server.run(0);
+//        System.out.println("Started test HTTP server on " + port);
+//        String serverUrl = "http://localhost:" + port;
+//        facade = new ServerFacade(serverUrl);
+//        facade.clear();
+//
+//        new Repl(serverUrl).run();
 //        ChessBoardPrinter printer = new ChessBoardPrinter();
 //        printer.printChessBoard(new ChessGame(), true);
 //        printer.printChessBoard(new ChessGame(), false);
